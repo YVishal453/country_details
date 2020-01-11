ff5125794ce21175125589f21daeda667541221e
Shael <shailendra@bitcot.com>
2020-01-11 16:24:02 +0530

    initial commit

---
+++ lib/country_detail.rb
@@ -0,0 +1,35 @@
+require 'rest-client'
+
+module CountryDetail
+
+  class Get
+    @@all_countries = []
+    def initialize
+      url = 'https://restcountries.eu/rest/v2/all'
+      rest_client_response = RestClient.get(url)
+      @@all_countries = JSON.parse(rest_client_response.body)
+    end
+
+    def all
+      @@all_countries
+    end
+
+    def details_by_name(country_name, options = {})
+      detail = @@all_countries.map{|detail| detail if detail["name"]==country_name.capitalize}.compact
+      return {'success': 'false', 'Error': 'Invalid country name!'} unless detail.present?
+      return detail
+    end
+   
+    def details_by_code(code, options = {})
+      code = code.upcase
+      if code.length == 2
+        detail = @@all_countries.map{|detail| detail if detail["alpha2Code"]==code}.compact
+      elsif code.length == 3
+        detail = all.map{|detail| detail if detail["alpha3Code"]==code}.compact 
+      end      
+      return {'success': 'false', 'Error': 'Invalid code!'} unless detail.present? 
+      return detail
+    end
+
+  end
+end
---
+++ lib/version.rb
@@ -0,0 +1,3 @@
+module CountryDetail
+  VERSION = '0.0.1'
+end 
